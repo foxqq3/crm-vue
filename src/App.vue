f8c8618f8dd97eaceea4695a24e1c643c96e7544
foxqq3 <dmitriifoxqq2@gmail.com>
2023-01-25 07:22:24 +0300

    new commit

--- src/App.vue
+++ src/App.vue
@@ -1,34 +1,29 @@
-<script setup>
-import HelloWorld from './components/HelloWorld.vue'
+<script>
+// import HelloWorld from './components/HelloWorld.vue'
+import RegistrForm from './components/RegistrForm.vue';
+import InputForm from './components/InputForm.vue';
+
+
+export default {
+  components: {
+    RegistrForm,
+    InputForm
+  },
+
+
+
+}
 </script>
 
 <template>
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="/vite.svg" class="logo" alt="Vite logo" />
-    </a>
-    <a class="btn" href="https://vuejs.org/" target="_blank">
-      <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
-    </a>
+  <div class="content">
+    <registr-form></registr-form>
+    <registr-form></registr-form>
+    <input-form></input-form>
   </div>
-  <HelloWorld msg="Vite + Vue" />
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
 </template>
 
 <style lang="scss">
-// @import './assets/style/style.scss';
 
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  background-color: color('cornflower-blue');
-}
-
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
 </style>
---
+++ src/components/RegistrForm.vue
@@ -0,0 +1,17 @@
+<script setup>
+// import HelloWorld from './components/HelloWorld.vue'
+
+</script>
+
+<template>
+  <div class="content">
+    <h1>testdsadsadsadsa</h1>
+  </div>
+  
+
+  <!-- <HelloWorld msg="Vite + Vue" /> -->
+</template>
+
+<style lang="scss">
+
+</style>
